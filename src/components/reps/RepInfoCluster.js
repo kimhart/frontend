3c10e71c34947fcc0a1475f8754cec1c19d4e4cf
Mattan Ingram <mattaningram@gmail.com>
2017-04-09 18:56:44 -0400

    added grade to card

--- src/components/reps/RepInfoCluster.js
+++ src/components/reps/RepInfoCluster.js
@@ -35,8 +35,8 @@ class RepInfoCluster extends React.Component {
             <p className="rep-info-role">{chamber.replace(/\b\w/g, l => l.toUpperCase())} {this.formatParty(party)}</p>
           </div>
           <div className="rep-letter-grade-wrap">
-            <p className="rep-letter-grade">{letter_grade}</p>
             <IconStamp />
+            <span className="rep-letter-grade">{letter_grade}</span>
           </div>
         </div>
       </div>
