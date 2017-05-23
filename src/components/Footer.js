8316e8f0fe72007732aa6a9ac24411ed72fcd9eb
kimhart <kim@jwplayer.com>
2017-05-22 21:57:55 -0400

    updated top nav styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,6 +17,7 @@ class Footer extends React.Component {
     if (location.href.includes('search')) return 'search';
     if (location.href.includes('rank')) return 'rank';
     if (location.href.includes('explore')) return 'explore';
+    if (location.href.includes('analyze')) return 'analyze';
     return false;
   }
 
@@ -35,33 +36,38 @@ class Footer extends React.Component {
       <nav className={`nav-bar ${placement}`}>
         {placement === 'top' &&
         <Link to="/" className="nav-logo nav-bar-tab">
-          <TallyLogo height="80px"/><span className="logo-text">Tally</span>
+          <TallyLogo /><span className="logo-text">Tally</span>
         </Link>
         }
-        <Link to="/" className={`nav-bar-tab ${!this.getActiveTab() ? ' active' : ''}`}>
-          <span className="nav-bar-tab_icon">
-            <IconReps fill="black" />
-          </span>
-          <span className="nav-bar-tab_title">Reps</span>
-        </Link>
-        <Link to="/rank" className={`nav-bar-tab ${this.getActiveTab() === 'rank' ? ' active' : ''}`} onClick={this.scrollToTop()}>
-          <span className="nav-bar-tab_icon">
-            <IconRank fill="black" />
-          </span>
-          <span className="nav-bar-tab_title" onClick={this.scrollToTop()}>Rank</span>
-        </Link>
-        <Link to="/analyze" className={`nav-bar-tab ${this.getActiveTab() === 'analyze' ? ' active' : ''}`}>
-          <span className="nav-bar-tab_icon">
-            <IconAnalyze fill="black" />
-          </span>
-          <span className="nav-bar-tab_title">Analyze</span>
-        </Link>
-        <Link to="/search" className={`nav-bar-tab ${this.getActiveTab() === 'search' ? ' active' : ''}`}>
-          <span className="nav-bar-tab_icon">
-            <IconSearch fill="black" />
-          </span>
-          <span className="nav-bar-tab_title">Search</span>
-        </Link>
+        <div className="main-tabs">
+          <Link to="/" className={`nav-bar-tab ${!this.getActiveTab() ? ' active' : ''}`}>
+            <span className="nav-bar-tab_icon">
+              <IconReps fill="black" />
+            </span>
+            <span className="nav-bar-tab_title">Reps</span>
+          </Link>
+          <Link to="/rank" className={`nav-bar-tab ${this.getActiveTab() === 'rank' ? ' active' : ''}`} onClick={this.scrollToTop()}>
+            <span className="nav-bar-tab_icon">
+              <IconRank fill="black" />
+            </span>
+            <span className="nav-bar-tab_title" onClick={this.scrollToTop()}>Rank</span>
+          </Link>
+          <Link to="/analyze" className={`nav-bar-tab ${this.getActiveTab() === 'analyze' ? ' active' : ''}`}>
+            <span className="nav-bar-tab_icon">
+              <IconAnalyze fill="black" />
+            </span>
+            <span className="nav-bar-tab_title">Analyze</span>
+          </Link>
+          <Link to="/search" className={`nav-bar-tab ${this.getActiveTab() === 'search' ? ' active' : ''}`}>
+            <span className="nav-bar-tab_icon">
+              <IconSearch fill="black" />
+            </span>
+            <span className="nav-bar-tab_title">Search</span>
+          </Link>
+        </div>
+        <div className="current-user nav-bar-tab">
+          <span className="current-user-name">Mattan  👤</span>
+        </div>
       </nav>
     );
   }
