04dac302d33d9012cf8b769ec6336f6cb6397e05
Mattan Ingram <mattaningram@gmail.com>
2017-09-03 21:12:14 -0400

    a lot of style fixes

--- src/components/reps/ReportCard.js
+++ src/components/reps/ReportCard.js
@@ -176,7 +176,7 @@ class ReportCard extends React.Component {
       <div className="rep-card-content">
         { this.props.close &&
           <div className="card-close" onClick={() => this.props.close()}>
-            <IconClose color="#4990E2"/>
+            <IconClose color="#3A7ADB"/>
           </div>
         }
         <div className="rep-card-header-wrap">
@@ -184,7 +184,7 @@ class ReportCard extends React.Component {
             <IconStamp fill="#3A7ADB" />
             <span className="rep-letter-grade">{letter_grade}</span>
           </div>
-          <div className="rep-card-photo" style={{background: `url(${this.getPhotoSource()}) no-repeat center 10% / cover`}} />
+          <div className="rep-card-photo" style={{background: `url(${this.getPhotoSource()}) no-repeat center 10% / cover`}}></div>
           <h1 className="rep-card-name">{ fullName }</h1>
           <div className="rep-card-position-wrap">
             <span className="rep-card-role">
@@ -192,51 +192,44 @@ class ReportCard extends React.Component {
             </span>
             { leadership_position !== "None" && <span className="rep-card-leadership">{ leadership_position }</span> }
           </div>
-          <button className={`contact-btn button--large button--outline button--blue`} onClick={() => this.setState({ contact: !contact })}>{contact ? <span className="back-btn"><IconAngleDown fill="#3A7ADB"/>Back</span> : 'Contact'}</button>
+          <button className={`contact-btn button--large button--outline button--blue`} onClick={() => this.setState({ contact: !contact })}>{contact ? <span className="button-contents"><IconAngleDown fill="#3A7ADB"/>Back</span> : 'Contact'}</button>
+
           { !contact && this.getMetricsTabs() }
-          { contact &&
-          <div className="contact-container">
-            { phone &&
-              <div className="contact-row">
-              <a target="_blank" href={`tel:${phone}`}>
-                <div className="contact-icon-circle"><IconPhone /></div>{this.formatPhone(phone)}
-              </a>
-            </div>
-            }
-            { website &&
-              <div className="contact-row">
-                <a target="_blank" href={website}>
-                  <div className="contact-icon-circle"><IconLink /></div>
-                  {this.formatUrl(website)}
-                </a>
-              </div>
-            }
-            { twitter_handle &&
-              <div className="contact-row">
-                <a target="_blank" href={`https://twitter.com/${twitter_handle}`}>
-                  <div className="contact-icon-circle"><IconTwitter /></div>
-                  {this.formatUrl(twitter_handle)}
-                </a>
-              </div>
-            }
-            { facebook &&
-              <div className="contact-row">
-                <a target="_blank" href={`https://${facebook}`}>
-                  <div className="contact-icon-circle"><IconFacebook /></div>{this.formatUrl(facebook)}
-                </a>
-              </div>
-            }
-            { address &&
-              <div className="contact-row">
-                <a target="_blank" href={`http://maps.google.com/?q=${this.formatAddress(address)}`}>
-                  <div className="contact-icon-circle"><IconLocation /></div>
-                  {this.formatAddress(address)}
-                </a>
-              </div>
-            }
-            </div>
+        </div>
+        { contact &&
+        <div className="contact-container">
+          { phone &&
+          <a className="contact-row" target="_blank" href={`tel:${phone}`}>
+            <div className="contact-icon-circle"><IconPhone /></div>
+            {this.formatPhone(phone)}
+          </a>
           }
-          </div>
+          { website &&
+          <a className="contact-row" target="_blank" href={website}>
+            <div className="contact-icon-circle"><IconLink /></div>
+            {this.formatUrl(website)}
+          </a>
+          }
+          { twitter_handle &&
+          <a className="contact-row" target="_blank" href={`https://twitter.com/${twitter_handle}`}>
+            <div className="contact-icon-circle"><IconTwitter /></div>
+            {this.formatUrl(twitter_handle)}
+          </a>
+          }
+          { facebook &&
+          <a className="contact-row" target="_blank" href={`https://${facebook}`}>
+            <div className="contact-icon-circle"><IconFacebook /></div>
+            {this.formatUrl(facebook)}
+          </a>
+          }
+          { address &&
+          <a className="contact-row" target="_blank" href={`http://maps.google.com/?q=${this.formatAddress(address)}`}>
+            <div className="contact-icon-circle"><IconLocation /></div>
+            {this.formatAddress(address)}
+          </a>
+          }
+        </div>
+        }
         { !contact && this.getCardContent() }
       </div>
     );
