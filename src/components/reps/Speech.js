9e39fbc153737c4a8ee8be6a8380362174f19199
kimhart <kim@jwplayer.com>
2017-09-13 22:41:41 -0400

    mapped speech search, added search input, need to handle search

--- src/components/reps/Speech.js
+++ src/components/reps/Speech.js
@@ -2,22 +2,42 @@ import React from 'react';
 import Relay from 'react-relay';
 import { isLoading } from '../../utils/Utils';
 import BeliefRange from './BeliefRange';
-import { IconAngleDown } from '../icons/Icons';
+import { IconAngleDown, IconSearch } from '../icons/Icons';
 
 class Speech extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      speech: []
+      speech: [],
+      search_terms: []
     };
-    props.relay.setVariables({ bioguide_id: props.bioguide_id }, ({ done, aborted, error }) => {
+    props.relay.setVariables({ bioguide_id: props.bioguide_id, search_terms: props.search_terms }, ({ done, aborted, error }) => {
       if (done || aborted || error) {
         isLoading(false);
       }
     });
     isLoading(true);
   }
+
+  // handleSearch = _.debounce(() => {
+  //   this.props.relay.setVariables({
+  //     search_terms: this.searchBox.value,
+  //     bioguide_id: this.props.bioguide_id
+  //   }, ({ aborted, done, error }) => {
+  //     if (aborted || done || error) {
+  //       isLoading(false);
+  //     }
+  //   });
+  //   isLoading(true);
+  // }, 300);
+
+  handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      console.log(this.searchBox.value)
+    }
+  }
+
 
   componentWillUpdate(nextProps, nextState) {
     if (nextProps.data.speech) {
@@ -41,10 +61,21 @@ class Speech extends React.Component {
   }
 
   render() {
+    const { search_terms } = this.state;
+    const lastName = this.props.name.split(',')[0];
     return (
       <div className="card-section-speech">
         <div className="card-section-header-wrap">
-          <span className="card-section-description">Most frequently spoken phrases throughout this session, according to official Congressional transcripts:</span>
+          <span className="card-section-description">Analysis of {lastName}'s speech on the floor this session, according to official Congressional transcripts. </span>
+        </div>
+        <div className="rank-search speech-search">
+          <input className="rank-search-filter no-focus-style" placeholder="Search keywords..." ref={c => this.searchBox = c} onKeyPress={(e) => this.handleSearch(e)}/>
+          <div className="search-bar-icon">
+            <IconSearch width="20px" fill="#3A7ADB" />
+          </div>
+        </div>
+        <div className="speech-search-header">
+          { search_terms.length > 0 ? `Results for ${search_terms}:` : "Most-spoken phrases:" }
         </div>
         <div className="speech-top-results-wrap">
           { this.renderSpeech() }
@@ -58,6 +89,7 @@ class Speech extends React.Component {
 export default Relay.createContainer(Speech, {
   initialVariables: {
     bioguide_id: null,
+    search_terms: null
   },
   fragments: {
     data: () => Relay.QL`
@@ -68,6 +100,12 @@ export default Relay.createContainer(Speech, {
         frequency
         rank
         word
+      }
+      search_speech(bioguide_id: $bioguide_id, search_terms: $search_terms) {
+        date
+        speaker
+        speaker_text
+        subject
       }
     }
   `
