c39456c1fedb5fcb4c091fb0d2e9536f3ea98c42
kimhart <kim@jwplayer.com>
2017-09-24 00:40:27 -0400

    formatted/styled subject text section, added a clear all tags button, made top phrases clickable/behave like search terms, added highlighter dependency

--- src/components/reps/Speech.js
+++ src/components/reps/Speech.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Relay from 'react-relay';
+import Highlight from 'react-highlighter';
 import { isLoading } from '../../utils/Utils';
 import BeliefRange from './BeliefRange';
 import { IconAngleDown, IconSearch, IconClose } from '../icons/Icons';
@@ -10,7 +11,8 @@ class Speech extends React.Component {
     super(props);
     this.state = {
       speech: [],
-      search_terms: []
+      search_terms: [],
+      view_subject: false
     };
     props.relay.setVariables({
       bioguide_id: props.bioguide_id
@@ -28,12 +30,17 @@ class Speech extends React.Component {
     }
   }
 
-  addSearchTerm = (e) => {
+  addSearchTerm = (e, topPhrase) => {
     e.preventDefault();
-    if (this.searchBox.value === "") {
+    const searchValue = this.searchBox.value;
+    if (!topPhrase && !searchValue) {
       return;
+    } else if (topPhrase && !searchValue) {
+      const search_terms = [topPhrase];
+      this.setState({ search_terms });
+      this.props.relay.setVariables({ search_terms });
     } else {
-      const search_terms = [ ...this.state.search_terms, this.searchBox.value ];
+      const search_terms = [ ...this.state.search_terms, searchValue ] || topPhrase;
       this.setState({ search_terms });
       this.props.relay.setVariables({ search_terms });
       this.searchForm.reset();
@@ -46,6 +53,19 @@ class Speech extends React.Component {
     });
     this.setState({ search_terms });
     this.props.relay.setVariables({ search_terms });
+  }
+
+  clearAllSearchTerms = () => {
+    this.setState({ search_terms: [] });
+    this.props.relay.setVariables({ search_terms: [] });
+  }
+
+  handleSubjectView = (view_subject) => {
+    this.setState({ view_subject })
+  }
+
+  backToSubjects = () => {
+    this.setState({ view_subject: false })
   }
 
   renderSearchPills = () => {
@@ -62,15 +82,18 @@ class Speech extends React.Component {
     });
   }
 
-  renderSpeech = () => {
+  renderTopPhrases = () => {
     const { speech } = this.state;
     if (!speech.length) {
       `We couldn't find this rep's top phrases. Check back soon!`
     } else {
       return speech.map(({ word, frequency, rank }, index) => (
-        <div key={`${word}${frequency}${rank}${index}`} className="rep-speech-list-item-wrap">
+        <div onClick={(e) => this.addSearchTerm(e, word)} key={`${word}${frequency}${rank}${index}`} className="rep-speech-list-item-wrap">
           <span className="rep-speech-list-item-text">"{ word }"</span>
-          <span className="rep-speech-list-item-frequency">{ `${frequency} ${frequency > 1 ? 'times' : 'time'}` }</span>
+          <div className="flex-align-center">
+            <span className="rep-speech-list-item-frequency">{ `${frequency} ${frequency > 1 ? 'times' : 'time'}` }</span>
+            <IconAngleDown width="10px" color="#8b8b8b" />
+          </div>
         </div>
       ))
     }
@@ -80,7 +103,7 @@ class Speech extends React.Component {
     const { search_speech } = this.props.data;
     const lastName = this.props.name.split(',')[0];
     return search_speech && search_speech.length > 0 ? search_speech.map(({ date, speaker, speaker_text, subject }, index) => (
-      <div key={`${date}${subject}${index}`} className="rep-speech-list-item-wrap">
+      <div onClick={() => this.handleSubjectView(search_speech)} key={`${date}${subject}${index}`} className="rep-speech-list-item-wrap">
         <span className="rep-speech-list-item-date">{new Date(date).toLocaleDateString()}</span>
         <span className="rep-speech-list-item-subject">{subject}</span>
         <IconAngleDown width="10px" color="#3A7ADB" />
@@ -89,33 +112,59 @@ class Speech extends React.Component {
     <div className="no-speech-search-results">We couldn't find any instances of {lastName} speaking about these subjects.</div>
   }
 
+  renderSubjectText = () => {
+    const { date, speaker, speaker_text, subject } = this.state.view_subject[0];
+    return (
+      <div>
+        <div className="rep-speech-subject-title">{subject}</div>
+        <div className="rep-speech-subject-date">{new Date(date).toLocaleDateString()}</div>
+        <div className="rep-speech-subject-text">
+          "<Highlight search={this.state.search_terms}>{speaker_text}</Highlight>"
+        </div>
+      </div>
+    );
+  }
+
   render() {
-    const { search_terms } = this.state;
+    const { search_terms, view_subject } = this.state;
     const lastName = this.props.name.split(',')[0];
     return (
       <div className="card-section-speech">
-        <div className="card-section-header-wrap">
-          <span className="card-section-description">Analysis of speech on the House and Senate floor, according to official Congressional transcripts.</span>
-        </div>
-        <div className="rank-search speech-search">
-          <form className="add-search-term-form" onSubmit={(e) => this.addSearchTerm(e)} ref={c => this.searchForm = c}>
-            <input className="rank-search-filter no-focus-style" placeholder="Search keywords..." ref={c => this.searchBox = c} />
-          </form>
-          <div className="search-bar-icon">
-            <IconSearch width="20px" fill="#3A7ADB" />
+        {!view_subject &&
+        <div>
+          <div className="card-section-header-wrap">
+            <span className="card-section-description">Analysis of speech on the House and Senate floor, according to official Congressional transcripts.</span>
+          </div>
+          <div className="rank-search speech-search">
+            <form className="add-search-term-form" onSubmit={(e) => this.addSearchTerm(e)} ref={c => this.searchForm = c}>
+              <input className="rank-search-filter no-focus-style" placeholder="Search keywords..." ref={c => this.searchBox = c} />
+            </form>
+            <div className="search-bar-icon">
+              <IconSearch width="20px" fill="#3A7ADB" />
+            </div>
           </div>
         </div>
-        { !search_terms.length &&
+        }
+        { !search_terms.length && !view_subject &&
           <div>
-            <div className="speech-search-header">Top Phrases this Session</div>
-            <div className="speech-top-results-wrap">{this.renderSpeech()}</div>
+            <div className="speech-search-header">Top Phrases Spoken this Session</div>
+            <div className="speech-top-results-wrap">{this.renderTopPhrases()}</div>
           </div>
         }
-        { search_terms.length > 0 &&
+        { search_terms.length > 0 && !view_subject &&
           <div>
             <div className="speech-pill-container">{this.renderSearchPills()}</div>
-            <div className="speech-search-header">Search Results</div>
+            <div className="clear-all-search-terms" onClick={() => this.clearAllSearchTerms()}>Clear All Tags</div>
+            <div className="speech-search-subheaders">
+              <span>Date</span><span>Subject</span>
+            </div>
             <div className="speech-search-results-wrap">{this.renderSearchResults()}</div>
+          </div>
+        }
+        { view_subject &&
+          <div className="speech-subject-wrap">
+            <div onClick={() => this.backToSubjects()} className="back-to-subjects"><IconAngleDown width="10px"/>Back</div>
+            {this.renderSubjectText()}
           </div>
         }
       </div>
